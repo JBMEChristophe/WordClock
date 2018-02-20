754946a2c4647b6e2217d4848d2d667541eefc8a
jchristophe <janou.christophe@gmail.com>
2018-02-20 13:22:56 +0100

    Added a reporting method for structurized reporting.

--- Clock/configuration.h
+++ Clock/configuration.h
@@ -7,4 +7,27 @@
 #define COLUMNS 11
 #define BRIGHTNESS 255
 
+//reporting is defined by levels 1 upto 5 with 5 reporting everything
+//when putting 0 in the reporting level nothing will be printed
+#define REPORTING_LEVEL 5
+
+#define DEBUG 5
+#define INFO 4
+#define WARN 3
+#define ERROR 2
+#define FATAL 1
+
+void reportln(String s, int severity) {
+	if(severity < REPORTING_LEVEL) {
+		if(severity == FATAL) Serial.print("FATAL: ");
+		else if (severity == ERROR)	Serial.print("ERROR: ");
+		else if (severity == WARN)	Serial.print("WARN: ");
+		else if (severity == INFO)	Serial.print("INFO: ");
+		else if (severity == DEBUG)	Serial.print("DEBUG: ");
+		else Serial.print("Undefined severity: ");
+		Serial.println(s);
+	}
+}
+
+
 #endif //CONFIGURATION_H
